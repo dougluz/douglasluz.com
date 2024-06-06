71fc6474a54eff81447a2808ac8fe05f9ea2867d
Douglas Luz <douglas.luz@rdstation.com>
2024-06-05 21:42:19 -0300

    chore: adde clsx to handle class names

--- components/linkItem.js
+++ components/linkItem.js
@@ -1,23 +1,16 @@
-import { Link, useColorModeValue } from "@chakra-ui/react"
 import NextLink from "next/link"
+import clsx from "clsx"
 
 export const LinkItem = ({ href, path, _target, children, ...props }) => {
   const active = path === href
-  const inactiveColor = useColorModeValue("black", "whiteAlpha.900")
-  const activeBackgroundColor = useColorModeValue("gray.400", "purple")
-  const activeColor = useColorModeValue("#000", "#fff")
   return (
     <NextLink href={href} passHref legacyBehavior>
-      <Link
-        p={2}
-        bg={active ? activeBackgroundColor : undefined}
-        color={active ? activeColor : inactiveColor}
-        _target={_target}
-        borderRadius={10}
-        {...props}
+      <a
+        className={clsx("p-2 rounded-md", active && "bg-gray-300 underline")}
+        target={_target}
       >
         {children}
-      </Link>
+      </a>
     </NextLink>
   )
 }
